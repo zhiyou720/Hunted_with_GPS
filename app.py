01844aabe117b2c09854d16723353473d5065ab2
zhangkun <zhiyou720@gmail.com>
2018-06-27 06:16:49 +0100

    User page init

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
 
 
 if __name__ == '__main__':
